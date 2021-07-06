bacb6fcf6f9959d195e1c8a6b43b00ab720b699d
Nizar7zak <nizar7zakout@gmail.com>
2021-07-06 21:18:19 +0300

    finish js function for nav scroll Relates #1

---
+++ script.js
@@ -0,0 +1,18 @@
+// const navBar = document.querySelector(".header-nav");
+// const sectionMain = document.querySelector(".section-main");
+
+
+// const sectionOneOptions = {
+//     roorMargin: "-200px 0px 0px 0px"
+// };
+
+// const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver){
+//     entries.forEach( (entry) => {
+//         if (!entry.isIntersecting) {
+//             navBar.classList.add("nav-scrolled")
+//         } else {
+//             navBar.classList.remove("nav-scrolled")
+//         }
+
+//     })
+// }, sectionOneOptions);
