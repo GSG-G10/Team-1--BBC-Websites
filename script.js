89fcf1ec1679db843dc2b044f7b8ded964420ba8
Nizar7zak <nizar7zakout@gmail.com>
2021-07-07 00:31:45 +0300

    finish responsive for all screen and write function on js to mobile navbar Relates #1

--- script.js
+++ script.js
@@ -15,4 +15,15 @@
 //         }
 
 //     })
-// }, sectionOneOptions);
+// }, sectionOneOptions)
+
+let navMobile = document.getElementById("nav-mobile");
+let ulMobile = document.getElementById("ulMob");
+let allNav = document.getElementById("active-nave")
+
+navMobile.onclick = function(){
+    ulMobile.classList.toggle("active");
+    allNav.classList.toggle("active-nav")
+    
+
+}
