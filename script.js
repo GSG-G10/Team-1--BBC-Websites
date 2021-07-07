7f395b4e1a7b25fc81aa59f9dde471fdc962199b
muhammadharoun <hro19502001@gmail.com>
2021-07-07 17:35:49 +0300

    fix

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// const navBar = document.querySelector(".header-nav");
-// const sectionMain = document.querySelector(".section-main");
-
-
-// const sectionOneOptions = {
-//     roorMargin: "-200px 0px 0px 0px"
-// };
-
-// const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver){
-//     entries.forEach( (entry) => {
-//         if (!entry.isIntersecting) {
-//             navBar.classList.add("nav-scrolled")
-//         } else {
-//             navBar.classList.remove("nav-scrolled")
-//         }
-
-//     })
-// }, sectionOneOptions)
-
 let navMobile = document.getElementById("nav-mobile");
 let ulMobile = document.getElementById("ulMob");
 let allNav = document.getElementById("active-nave")
