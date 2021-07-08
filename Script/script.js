ace8febcca4515919b851438bbb6ff326b4d6425
Noorin99 <noorin.sa.99@gmail.com>
2021-07-08 12:06:26 +0500

    fix formatting problems in code relates #23

--- Script/script.js
+++ Script/script.js
@@ -2,9 +2,7 @@ const navMobile = document.getElementById("nav-mobile");
 const ulMobile = document.getElementById("ulMob");
 const allNav = document.getElementById("active-nave")
 
-navMobile.onclick = function(){
+navMobile.onclick = function () {
     ulMobile.classList.toggle("active");
     allNav.classList.toggle("active-nav")
-    
-
 }
